ab8ca02150546454e97206e30176c6b3474710c5
Ferry <you@example.com>
2022-04-11 15:05:06 +0100

    Create new user form module

---
+++ frontend/grupomaniasocial/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import NewUserForm from './NewUserForm';
+
+function App() {
+  return (
+    <NewUserForm></NewUserForm>
+  );
+}
+
+export default App;
