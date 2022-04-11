077daa46fdf7c8a4484b423bd972f344749a6b5d
Ferry <you@example.com>
2022-04-11 10:33:45 +0100

    Add existing file

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,44 +1,44 @@
 
-const { Sequelize, Model} = require('sequelize');
+const { Sequelize, Model } = require('sequelize');
 
 
 module.exports = (sequelize) => {
-return  User = sequelize.define('User', {
-   /*id:{
-        type: Sequelize.STRING,
-        allowNull: false,
-        unique: true,
-        primaryKey: true
-    },*/
-    email:{
-        type: Sequelize.STRING,
-        allowNull: false,
-        unique: true,
+    return User = sequelize.define('User', {
+        /*id:{
+             type: Sequelize.STRING,
+             allowNull: false,
+             unique: true,
+             primaryKey: true
+         },*/
+        email: {
+            type: Sequelize.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        password: {
+            type: Sequelize.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        /*position:{
+            type: Sequelize.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        
+        deparment:{
+            type: Sequelize.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        
+        photo:{
+            type: Sequelize.STRING,
+            allowNull: false,
+            unique: true,
+        },*/
     },
-    password:{
-        type: Sequelize.STRING,
-        allowNull: false,
-        unique: true,
-    },
-    /*position:{
-        type: Sequelize.STRING,
-        allowNull: false,
-        unique: true,
-    },
-    
-    deparment:{
-        type: Sequelize.STRING,
-        allowNull: false,
-        unique: true,
-    },
-    
-    photo:{
-        type: Sequelize.STRING,
-        allowNull: false,
-        unique: true,
-    },*/
-},
-{
-    timestamps: false
-  });
+        {
+            timestamps: false
+        });
 }
